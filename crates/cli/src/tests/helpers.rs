cd82e898bb40a68b34bd9cd2a998622c2aabd2e2
12345431 <xiaozunzun404@gmail.com>
2026-06-07 12:13:02 +0800

    De

--- crates/cli/src/tests/helpers.rs
+++
@@ -1,4 +0,0 @@
-pub use crate::fuzz::allocations;
-pub mod edits;
-pub(super) mod fixtures;
-pub(super) mod query_helpers;
--- crates/language/wasm/include/ctype.h
+++
@@ -1,8 +0,0 @@
-#ifndef TREE_SITTER_WASM_CTYPE_H_
-#define TREE_SITTER_WASM_CTYPE_H_
-
-static inline int isprint(int c) {
-  return c >= 0x20 && c <= 0x7E;
-}
-
-#endif // TREE_SITTER_WASM_CTYPE_H_
