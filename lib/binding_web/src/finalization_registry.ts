b8e3f6cc56f0bb2578dc0b32269628433ff83231
12345431 <xiaozunzun404@gmail.com>
2026-06-07 12:14:27 +0800

    Hi

--- lib/binding_web/src/finalization_registry.ts
+++
@@ -1,8 +0,0 @@
-export function newFinalizer<T>(handler: (value: T) => void): FinalizationRegistry<T> | undefined {
-  try {
-    return new FinalizationRegistry(handler);
-  } catch(e) {
-    console.error('Unsupported FinalizationRegistry:', e);
-    return;
-  }
-}
